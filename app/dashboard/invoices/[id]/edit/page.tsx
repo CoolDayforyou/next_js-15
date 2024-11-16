3e4257200873a03e47739011b08619acd34ecbef
CoolDayforyou <cooldayforyou@gmail.com>
2024-11-16 16:54:01 +0200

    Completed the Next Dashboard course

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,41 @@
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import EditInvoiceForm from "@/app/ui/invoices/edit-form";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
+
+const EditPage = async (props: { params: Promise<{ id: string }> }) => {
+  const { id } = await props.params;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditInvoiceForm invoice={invoice} customers={customers} />
+    </main>
+  );
+};
+
+export default EditPage;
---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,28 @@
+import { fetchCustomers } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/create-form";
+import { Metadata } from "next";
+import React from "react";
+export const metadata: Metadata = {
+  title: "Create Invoice",
+};
+const CreatePage = async () => {
+  const customers = await fetchCustomers();
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+};
+
+export default CreatePage;
